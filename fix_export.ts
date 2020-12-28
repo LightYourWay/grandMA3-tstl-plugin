4e48f17ecd0d0768ab03a9d4b14835f225a40733
Lukas Runge <office@lukasrunge.de>
2020-12-28 18:00:45 +0100

    ✨ added first working version of fix_export plugin

---
+++ fix_export.ts
@@ -0,0 +1,21 @@
+import * as ts from "typescript";
+import * as tstl from "typescript-to-lua";
+
+const plugin: tstl.Plugin = {
+  visitors: {
+    [ts.SyntaxKind.ExportAssignment]: (node, context) => {
+      const expressions: tstl.Expression[] = [];
+
+      if (ts.isArrayLiteralExpression(node.expression)) {
+        for (const element of node.expression.elements) {
+          const expression = context.transformExpression(element);
+          expressions.push(expression);
+        }
+      }
+
+      return tstl.createReturnStatement(expressions, node);
+    }
+  }
+};
+
+export default plugin;
