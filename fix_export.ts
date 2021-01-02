c89203f6a7bb87fbcee2d12dd3bb80679dd8233e
Lukas Runge <office@lukasrunge.de>
2021-01-02 18:55:53 +0100

    👕 npm run lint:fix

--- fix_export.ts
+++ fix_export.ts
@@ -1,21 +1,21 @@
-import * as ts from "typescript";
-import * as tstl from "typescript-to-lua";
+import * as ts from 'typescript';
+import * as tstl from 'typescript-to-lua';
 
 const plugin: tstl.Plugin = {
-  visitors: {
-    [ts.SyntaxKind.ExportAssignment]: (node, context) => {
-      const expressions: tstl.Expression[] = [];
+	visitors: {
+		[ts.SyntaxKind.ExportAssignment]: (node, context) => {
+			const expressions: tstl.Expression[] = [];
 
-      if (ts.isArrayLiteralExpression(node.expression)) {
-        for (const element of node.expression.elements) {
-          const expression = context.transformExpression(element);
-          expressions.push(expression);
-        }
-      }
+			if (ts.isArrayLiteralExpression(node.expression)) {
+				for (const element of node.expression.elements) {
+					const expression = context.transformExpression(element);
+					expressions.push(expression);
+				}
+			}
 
-      return tstl.createReturnStatement(expressions, node);
-    }
-  }
+			return tstl.createReturnStatement(expressions, node);
+		},
+	},
 };
 
 export default plugin;
